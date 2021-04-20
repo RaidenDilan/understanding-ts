68cf671843ee0aac6c0948ac666bec3eca4882e8
RaidenDilan <raiden18@me.com>
2021-04-20 22:21:09 +0100

    Object Types + Nested Objects & Types

--- app.ts
+++ app.ts
@@ -1,14 +1,31 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') throw new Error('Incorrect input!');
-  const result = n1 + n2;
-  if (showResult) console.log(phrase + result);
-  else return result;
+const person: {
+  name: string,
+  age: number
+} = {
+// const person = {
+  name: 'Raiden',
+  age: 27
+};
+
+console.log(person.name);
+
+const product: {
+  id: string,
+  price: number,
+  tags: string[],
+  details: {
+    title: string,
+    description: string,
+  }
+} = {
+// const product = {
+  id: 'abc1',
+  price: 12.99,
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red Carpet',
+    description: 'A great carpet - almost brand-new!'
+  }
 }
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-let resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(product);
