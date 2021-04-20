1e0120905ab5439cc69796832bf9606beca77c22
RaidenDilan <raiden18@me.com>
2021-04-20 22:57:52 +0100

    Union Types

--- app.ts
+++ app.ts
@@ -1,40 +1,21 @@
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
+/*
+ * UNION TYPES
+ * on runtime check we tell TS that we are fine with either a number or string, we use:
+ * number | string
+ */
 
-enum Role {
-  ADMIN = 'ADMIN',
-  READ_ONLY = 100,
-  AUTHOR = 'AUTHOR'
-};
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  
+  if (typeof input1 === 'number' && typeof input2 === 'number' ) result = input1 + input2;
+  else result = input1.toString() + input2.toString();
 
-// const person: {
-//   name: string,
-//   age: number,
-//   hobbies: string[]
-//   role: [number, string], // marking a tupel, fixed-length array
-// } = {
-const person = {
-  name: 'Raiden',
-  age: 27,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
-};
-
-// person.role.push('admin'); // TS doesn't catch this error when using tupels becuase in TS the push method is an exception.
-// person.role[1] = 10;
-
-// person.role = [0, 'admin']; // this is ALLOWED, hence no TS errors in our IDE.
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports', 'Cooking'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+  return result;
 }
 
-if (person.role === Role.ADMIN) {
-  console.log('is admin');
-}
+
+const combinedAges = combine(30, 30)
+console.log(combinedAges);
+
+const combinedNames = combine('Raiden', 'Dilan')
+console.log(combinedNames);
